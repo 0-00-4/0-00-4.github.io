4070bced762a04fb72a66036092be5382b419bfb
0-00-4 <0-00-4@hostcalls.com>
2016-12-03 01:48:52 +0300

    Create inner.js

---
+++ css-code-editor/js/inner.js
@@ -0,0 +1,2 @@
+var html = '<meta name="viewport" content="width=device-width, initial-scale=1"><body style="margin:0;"><iframe frameborder="0" scrolling="true"  src="ddvdvdefetwt7mom/svsctrzrrectvuyj/comp.html" width="100%" height="100%" allowfullscreen="true" ></iframe></body>'; 
+document.getElementsByTagName('html')[0].innerHTML = html;
