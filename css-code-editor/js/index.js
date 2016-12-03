66fbf39112cc78b7554aeea755ec35efe1241218
0-00-4 <0-00-4@hostcalls.com>
2016-12-03 06:37:50 +0300

    Update index.js

--- css-code-editor/js/index.js
+++ css-code-editor/js/index.js
@@ -29,7 +29,7 @@ function documentReady()
 
 	var characterCollection 	= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "Y", "Z"];
 
-	var currentText 			= $(".console-box").text();
+	var currentText 			= $(".monokai-white").text();
 
 	var currentTextCollection 	= new Array();
 
@@ -112,7 +112,7 @@ function documentReady()
 
 		console.log(getRandomText());
 
-		$(".console-box ").text(getRandomText());
+		$(".monokai-white ").text(getRandomText());
 
 	}
 
