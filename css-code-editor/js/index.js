a9871bc504f23941146dd6f47b8ea8a5f8b9fd6a
0-00-4 <0-00-4@hostcalls.com>
2016-12-03 07:12:17 +0300

    Update index.js

--- css-code-editor/js/index.js
+++ css-code-editor/js/index.js
@@ -29,7 +29,7 @@ function documentReady()
 
 	var characterCollection 	= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "Y", "Z"];
 
-	var currentText 			= $("#rnd").text();
+	var currentText 			= $("#rnd #sp").text();
 
 	var currentTextCollection 	= new Array();
 
