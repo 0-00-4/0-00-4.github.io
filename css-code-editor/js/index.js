1ba2f79c299e9b728d0c7202e979f549addd355d
0-00-4 <0-00-4@hostcalls.com>
2016-12-03 08:50:02 +0300

    Update index.js

--- css-code-editor/js/index.js
+++ css-code-editor/js/index.js
@@ -1,41 +1,3 @@
-'use strict';
-
-window.app = function (_) {
-
-  var log = console.log.bind(console),
-      messageElement = document.querySelector('#js-typewriter'),
-      text = messageElement.innerText.trim();
-
-  var words = text.split(' ');
-
-  var work = [];
-
-  words.forEach(function (word) {
-    var splitWord = word.split('').map(function (char, index) {
-      return '<i>' + char + '</i>';
-    }).join('');
-    work.push(splitWord);
-  });
-
-  var formattedWords = work.map(function (word, index) {
-    return '<span>' + word + '</span>';
-  }).join(' ');
-
-  messageElement.innerHTML = formattedWords;
-  messageElement.classList.add('animate');
-
-  return {
-    replay: function replay(event) {
-      messageElement.classList.remove('animate');
-      messageElement.offsetHeight; // force reflow
-      setTimeout(function (_) {
-        messageElement.classList.add('animate');
-      });
-    }
-  };
-}();
-
-
 
 
 
@@ -148,6 +110,44 @@ function documentReady()
 
 
 
+
+
+'use strict';
+
+window.app = function (_) {
+
+  var log = console.log.bind(console),
+      messageElement = document.querySelector('#js-typewriter'),
+      text = messageElement.innerText.trim();
+
+  var words = text.split(' ');
+
+  var work = [];
+
+  words.forEach(function (word) {
+    var splitWord = word.split('').map(function (char, index) {
+      return '<i>' + char + '</i>';
+    }).join('');
+    work.push(splitWord);
+  });
+
+  var formattedWords = work.map(function (word, index) {
+    return '<span>' + word + '</span>';
+  }).join(' ');
+
+  messageElement.innerHTML = formattedWords;
+  messageElement.classList.add('animate');
+
+  return {
+    replay: function replay(event) {
+      messageElement.classList.remove('animate');
+      messageElement.offsetHeight; // force reflow
+      setTimeout(function (_) {
+        messageElement.classList.add('animate');
+      });
+    }
+  };
+}();
 
 
 
