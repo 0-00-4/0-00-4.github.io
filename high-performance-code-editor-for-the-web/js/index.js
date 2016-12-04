5751159e1067da900690a361b80160a75773a532
0-00-4 <0-00-4@hostcalls.com>
2016-12-04 21:10:36 +0300

    Update index.js

--- high-performance-code-editor-for-the-web/js/index.js
+++ high-performance-code-editor-for-the-web/js/index.js
@@ -1,4 +1,4 @@
  var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
+    editor.setTheme("ace/theme/ambiance");
     editor.getSession().setMode("ace/mode/javascript");
     document.getElementById('editor').style.fontSize='16px';
